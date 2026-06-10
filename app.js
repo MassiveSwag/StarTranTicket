0b2c3b78876332082349e3c687e96f08a1451b78
MassiveSwag <Dick_E_Rhodes@Hotmail.com>
2026-06-09 20:56:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-/*
-function toggleView() {
-			const one = document.getElementById('section-one');
-			const two = document.getElementById('section-two');
-			
-			if (one.style.display === 'none') {
-				one.style.display = 'block';
-				two.style.display = 'none';
-			} else {
-				one.style.display = 'none';
-				two.style.display = 'block';
-			}
-		}
-
-async function loadContent() {
-			try {
-				const [response1, response2] = await Promise.all([
-					fetch('indexPass.html'),
-					fetch('indexTicket.html')
-			    ]);
-				
-				const html1 = await response1.text();
-				const html2 = await response2.text();
-				
-				document.getElementById('section-one').innerHTML = html1;
-				document.getElementById('section-two').innerHTML = html2;
-			} catch (error) {
-				console.error('Error loading pages:', error);
-			}
-		}
-        
-    
-document.addEventListener('DOMContentLoaded', () => {
-    loadContent(); 
-)};
-*/
 
 function showPass() {
     document.getElementById('contentFrame').src = 'indexPass.html';
@@ -42,9 +6,6 @@ function showPass() {
 function showTicket() {
     document.getElementById('contentFrame').src = 'indexTicket.html';
 }
-document.addEventListener('DOMContentLoaded', () => {
-    showPass();
-});
 
 
     
